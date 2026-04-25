5429dff4ca663ed19f0e47bef0d45e3734372281
Chriselle Bijoy <bchriselle08@gmail.com>
2026-04-25 22:59:08 +0530

    Implement stack class with array
    Refactored stack implementation into a class with push, pop, and display methods.

--- C++/CP/Exp9/9a.cpp
+++ C++/CP/Exp9/9a.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
-#include <stack>
 using namespace std;
-int main(){
-    stack<int> st;
-    st.push(10);
-    st.push(5);
-    
-    // Accessing top element
-    cout << "Top element: " << st.top() << endl;
-    
-    // Popping an element
-    st.pop();
-    cout << "Top element after pop: " << st.top() << endl;
+
+class Stack {
+    int *arr, size, top;        
+
+public:
+    Stack(int s) {
+        size = s;
+        arr = new int[size];
+        top = -1;
+    }
+    void push(int x) {
+        if (top == size - 1) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        arr[++top] = x;
+    }
+    int pop() {
+        if (top == -1) {
+            cout << "Stack Underflow\n";
+            return -1;
+        }
+        return arr[top--];
+    }
+    bool isEmpty() {
+        return top == -1;
+    }
+    bool isFull() {
+        return top == size - 1;
+    }
+    void disp(){
+        for (int i = 0; i <= top; i++)
+            cout << arr[i] << " ";
+    }
+};
+int main() {
+    myStack st(4);
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    cout << "Popped: " << st.pop() << "\n";
+    st.disp();
     return 0;
 }
